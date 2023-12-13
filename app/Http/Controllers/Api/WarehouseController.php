6e2a63800c4945ad7c146507f831e3f5b620ab4d
HASSANGHANNAM <ITE.HASSANG@gmail.com>
2023-12-13 21:44:40 +0300

    xxx

--- app/Http/Controllers/Api/WarehouseController.php
+++ app/Http/Controllers/Api/WarehouseController.php
@@ -28,10 +28,6 @@ class WarehouseController extends Controller
             "city_id" => "required|integr",
             "location" => "required|max:45|string"
         ]);
-
-
-
-
         $locationData = [
             'city_id' => $request->city_id,
             'address' => $request->location,
